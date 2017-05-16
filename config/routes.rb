560bf05d9249f0cdf90bec099694315f7b9e945a
Arthur Renaud <arthurrenaud.pro@gmail.com>
2017-05-16 11:47:54 +0200

    Generate controller, views and index, show routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # Read
+  get 'tasks', to: 'tasks#index'
+  # get 'tasks/new', to: 'tasks#new'
+
+  # # Create
+  get 'tasks/:id', to: 'tasks#show'
+  # post 'tasks', to: 'tasks#create'
+
+  # # Update
+  # get 'tasks/:id/edit', to: 'tasks#edit'
+  # put 'tasks/:id', to: 'tasks#update'
+
+  # # Delete
+  # delete 'tasks/:id', to: 'tasks#destroy'
+
+  # resources :tasks
 end
